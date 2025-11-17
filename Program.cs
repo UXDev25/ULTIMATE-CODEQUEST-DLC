ddaff090db46a81d5c14f415fe868b838608ccfb
Unai Almiñana <unai.alminana.7e9@itb.cat>
2025-11-17 13:24:24 +0100

    Added main

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public class Program
+{
+    static void Main()
+    {
+        const string MenuTitle = "===== MAIN MENU - CODEQUEST DLC =====";
+        const string MenuOption1 = "1. Train your wizard";
+        const string MenuOption2 = "2. Increase LVL";
+        const string MenuOption3 = "3. Loot the mine";
+        const string MenuOption4 = "4. Show inventory";
+        const string MenuOption5 = "5. Buy items";
+        const string MenuOption6 = "6. Show attacks by LVL";
+        const string MenuOption7 = "7. Decode ancient Scroll";
+        const string MenuOptionExit = "0. Exit game";
+        const string MenuPrompt = "Choose an option (1-7) - (0) to exit: ";
+        const string InputErrorMessage = "Invalid input. Please enter a number between 0 and 7.";
+
+        int op = -1;
+        bool validInput = false;
+
+        var rand = new Random();
+
+        do
+        {
+            Console.WriteLine(MenuTitle);
+            Console.WriteLine(MenuOption1);
+            Console.WriteLine(MenuOption2);
+            Console.WriteLine(MenuOption3);
+            Console.WriteLine(MenuOptionExit);
+            Console.Write(MenuPrompt);
+            try
+            {
+                op = Convert.ToInt32(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+
+                    case 5:
+
+                        break;
+                    case 6:
+
+                        break;
+                    case 7:
+
+                        break;
+                    case 0:
+                        Console.WriteLine("Quitting...");
+                        break;
+                    default:
+                        Console.WriteLine(InputErrorMessage);
+                        break;
+
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(InputErrorMessage);
+                validInput = false;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(InputErrorMessage);
+                validInput = false;
+            }
+        } while (op != 0);
+    }
+}
